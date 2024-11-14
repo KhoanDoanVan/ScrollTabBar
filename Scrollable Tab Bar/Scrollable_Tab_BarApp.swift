63b74eaa0dc13f1c3bcd17ce0bca08f211baecf0
khoanisdev <doanvankhoan124@gmail.com>
2024-11-14 15:41:56 +0700

    Initial Commit

---
+++ Scrollable Tab Bar/Scrollable_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrollable_Tab_BarApp.swift
+//  Scrollable Tab Bar
+//
+//  Created by Đoàn Văn Khoan on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Scrollable_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
